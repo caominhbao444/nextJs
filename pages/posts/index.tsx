f58c25232a0a0d1357a976b4a4370df1428794df
caominhbao444 <minhbao11b3@gmail.com>
2023-09-15 17:16:00 +0700

    third

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,34 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 import React from 'react'
 
-export interface PostListPageProps {}
+export interface PostListPageProps {
+  posts: any[]
+}
 
-export default function PostListPage(props: PostListPageProps) {
-  return <div>BAo</div>
+export default function PostListPage({ posts }: PostListPageProps) {
+  console.log('hi', posts)
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Post Page</h1>
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext,
+) => {
+  console.log('static props')
+  const response = await fetch('https://648fbcb275a96b6644454d77.mockapi.io/api/car/posts')
+  const data = await response.json()
+  console.log(data)
+  return {
+    props: {
+      posts: data,
+    },
+  }
 }
